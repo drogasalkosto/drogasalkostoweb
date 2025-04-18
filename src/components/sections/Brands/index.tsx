648b8f2740ca679a125292bf9e9b0a86d2b08663
drogasalkosto <drogasalkostooficial@gmail.com>
2025-04-18 11:41:10 -0500

    first commit

---
+++ src/components/sections/Brands/index.tsx
@@ -0,0 +1,59 @@
+"use client";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import Image from "next/image";
+import { Autoplay } from "swiper/modules";
+import style from "./styles/styles.module.css";
+const brandsImages = [
+  "/brands/brands1.png",
+  "/brands/brands2.png",
+  "/brands/brands3.png",
+  "/brands/brands1.png",
+  "/brands/brands2.png",
+  "/brands/brands3.png",
+];
+
+const Brands = () => {
+  return (
+    <Swiper
+      modules={[Autoplay]}
+      spaceBetween={2}
+      style={{
+        width: "90vw",
+        margin: "0 auto",
+        height: "8rem",
+        padding: "1rem",
+      }}
+      slidesPerView={3}
+      loop={true}
+      autoplay={{
+        delay: 3000,
+      }}
+    >
+      {brandsImages.map((image, index) => (
+        <SwiperSlide className={style.slider} key={index}>
+          <div className={style.containerSectionImage}>
+            <section
+              style={{
+                width: "7rem",
+                height: "7rem",
+                borderRadius: "50%",
+              }}
+            >
+              <Image
+                src={image}
+                alt={`Brands${index + 1}`}
+                fill
+                style={{
+                  objectFit: "contain",
+                }}
+              />
+            </section>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default Brands;
---
+++ src/components/sections/Header/index.tsx
@@ -0,0 +1,95 @@
+"use client";
+import Image from "next/image";
+import style from "./styles/styles.module.css";
+import { montserrat } from "@/app/fonts";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import Brands from "../Brands";
+
+const images = [
+  "/assets/foto2.jpeg",
+  "/assets/foto1.jpeg",
+  "/assets/foto3.jpeg",
+  "/assets/foto4.jpeg",
+  "/assets/foto5.jpeg",
+  "/assets/foto5.jpeg",
+];
+
+const Header = () => {
+  return (
+    <header className={style.header}>
+      <Swiper
+        modules={[Autoplay]}
+        autoplay={{
+          delay: 2000,
+          disableOnInteraction: false,
+          pauseOnMouseEnter: true,
+          reverseDirection: true,
+          waitForTransition: true,
+        }}
+        style={{
+          height: "100%",
+          width: "100%",
+          position: "absolute",
+          zIndex: "-99999",
+        }}
+      >
+        {" "}
+        {images.map((image, index) => (
+          <SwiperSlide key={index}>
+            <div className={style.containerWraper}></div>
+            <Image
+              src={image}
+              fill
+              style={{
+                objectFit: "cover",
+                zIndex: -99999999999,
+                width: "100%",
+                height: "100%",
+              }}
+              loading="lazy"
+              alt="logo"
+              sizes="(max-width: 640px) 100vw, (max-width: 768px) 75vw, (max-width: 1024px) 50vw, 40vw"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+      <div className={style.conatinerHeader}>
+        <div className={style.logoContainer}>
+          <div className={style.containerLogoCompleto}>
+            <Image
+              src={"/logo-completo3.png"}
+              fill
+              style={{
+                objectFit: "contain",
+              }}
+              loading="lazy"
+              alt="logo-completo"
+              sizes="(max-width: 640px) 100vw, (max-width: 768px) 75vw, (max-width: 1024px) 50vw, 40vw"
+            />
+          </div>
+        </div>
+
+        <div className={style.containerTextHeader}>
+          <h2 className={montserrat.className}>precios competitivos</h2>
+          <h2 className={montserrat.className}>Variedad de medicamentos</h2>
+
+          <p>
+            Drogas Alkosto es tu aliado de confianza para el cuidado de tu salud
+            y bienestar. En nuestra farmacia encontrarás una amplia variedad de
+            medicamentos de alta calidad, productos de cuidado personal,
+            suplementos nutricionales, y artículos de salud, todo a precios
+            competitivos.
+          </p>
+          <button>Contactar ahora</button>
+        </div>
+        <div>
+          <Brands />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
